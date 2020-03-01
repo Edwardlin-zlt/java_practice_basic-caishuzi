58b3b3172f06e4262141cf019ac3a34b17089bc1
Edwardlin-zlt <edwardlin.zlt@gmail.com>
2020-03-01 17:38:13 +0800

    初步实现

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -5,20 +5,52 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class App {
 
     private static Random r = new Random();
+    private static Scanner sc = new Scanner(System.in);
+    private final static String[] CHINESE_NUMBER = {"一", "二", "三", "四", "五", "六"};
 
     public static void main(String[] args) {
         // 1. getAnswer
         List<Integer> answer = getAnswer();
         GameJudger gameJudger = new GameJudger(answer);
+        for (int i = 0; i < 6; i++) {
+            String userAnswerStr = sc.next();
+            List<Integer> userAnswer = parseAnswerInput(userAnswerStr);
+            try {
+                if (gameJudger.judgeAnswer(userAnswer)) {
+                    System.out.println(userAnswerStr + " // 第" + CHINESE_NUMBER[i] + "次");
+                    System.out.println(gameJudger.getGuessHistoryDescribe());
+                    System.out.print("You Win!!!!!!!");
+                    return;
+                } else {
+                    System.out.println(userAnswerStr + " // 第" + CHINESE_NUMBER[i] + "次");
+                    System.out.println(gameJudger.getGuessHistoryDescribe());
+                }
+            } catch (WrongInputException e) {
+                i--;
+                System.out.println(userAnswerStr);
+                System.out.println(gameJudger.getGuessHistoryDescribe());
+            }
+        }
+        System.out.print("Unfortunately, you have no chance, the answer is " + answer + "");
+    }
+
+
+    private static List<Integer> parseAnswerInput(String answerStr) {
+        List<Integer> answer = new ArrayList<>();
+        for (char c : answerStr.toCharArray()) {
+            answer.add(Character.getNumericValue(c));
+        }
+        return answer;
     }
 
     public static List<Integer> getAnswer() {
         List<Integer> answer = new ArrayList<>();
-        final String answerFile = "src/main/resources/answer.txt.wrong";
+        final String answerFile = "src/main/resources/answer.txt";
         try (FileInputStream fileInputStream = new FileInputStream(answerFile)) {
             StringBuilder fileContent = new StringBuilder();
             byte[] buffer = new byte[1024];
@@ -28,7 +60,7 @@ public class App {
             }
             String answerStr = fileContent.toString().trim();
             char[] chars = answerStr.toCharArray();
-            for (Character c: chars) {
+            for (Character c : chars) {
                 answer.add(Character.getNumericValue(c));
             }
         } catch (IOException e) {
--- src/main/java/com/thoughtworks/GameJudger.java
+++ src/main/java/com/thoughtworks/GameJudger.java
@@ -1,46 +1,64 @@
 package com.thoughtworks;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class GameJudger {
     private List<Integer> answer;
     private List<List<Integer>> guessHistory = new LinkedList<>();
-    private String curDescribe = "";
     private String guessHistoryDescribe = "";
 
     public GameJudger(List<Integer> answer) {
         this.answer = answer;
     }
 
+    public String getGuessHistoryDescribe() {
+        return guessHistoryDescribe;
+    }
+
+    public List<Integer> getAnswer() {
+        return answer;
+    }
+
     public boolean judgeAnswer(List<Integer> userAnswer) throws WrongInputException {
         // 1. 合法输入
-        if (userAnswer.size() != 4) {
+        checkInputAnswer(userAnswer);
+        // 2. judge A B
+        byte aTypeAnswerCounter = 0;
+        byte bTypeAnswerCounter = 0;
+        for (int i = 0; i < userAnswer.size(); i++) {
+            int curValue = userAnswer.get(i);
+            if (answer.get(i).equals(curValue)){
+                aTypeAnswerCounter++;
+            } else if (answer.contains(curValue)){
+                bTypeAnswerCounter++;
+            }
+        }
+
+        String curDescribe = aTypeAnswerCounter+ "A" + bTypeAnswerCounter + "B";
+        updateHistory(userAnswer, curDescribe);
+        return aTypeAnswerCounter == 4;
+    }
+
+    private void checkInputAnswer(List<Integer> userAnswer) throws WrongInputException {
+        Set<Integer> userAnswerSet = new HashSet<>();
+        for (Integer integer : userAnswer) {
+            userAnswerSet.add(integer);
+        }
+        if (userAnswerSet.size() != 4) {
+            updateHistory(userAnswer, "Wrong input");
             throw new WrongInputException("Wrong input");
         }
-        // 2. judge A B
-        byte aTypeAnswerCounter;
-        byte bTypeAnswerCounter;
-        for (int i = 0; i < userAnswer.size(); i++) {
-            if (answer.)
-        }
-
-
-        // 5. return boolean
-        for (Integer integer : userAnswer) {
-
-        }
-        // 5. put into guessHistory
-        updateHistory(userAnswer);
-        return true;
     }
 
     private void updateHistory(List<Integer> userAnswer, String curDescribe) {
         guessHistory.add(userAnswer);
-
-        // update curDescribe
-
-        // update guessHistoryDescribe
+        StringBuilder userAnswerStr = new StringBuilder();
+        for (Integer integer : userAnswer) {
+            userAnswerStr.append(integer);
+        }
+        guessHistoryDescribe += userAnswerStr.append(" ").append(curDescribe).append("\n");
     }
 }
